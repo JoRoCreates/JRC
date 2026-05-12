aef96c63044de4d8457588e81fdbc939900e17d3
jaerog <jaerrogers@gmail.com>
2026-05-12 00:25:21 -0500

    add tabs and cards with mock data

---
+++ Readme.md
@@ -0,0 +1,33 @@
+Notes
+-- can use the etsy image url or store images in /image and use that url in the different product.json files
+
+TO DO:
+
+1. Add Logo in images as logo.png
+
+To Run:
+
+1. In powershell git clone the repo
+
+```
+git clone https://github.com/JoRoCreates/JRC.git
+```
+
+2. Open in vscode with code.
+
+```
+cd JoRoCreates
+code .
+```
+
+3. To run
+
+```
+python -m http.server
+```
+
+Then open in browser or in vscode:
+
+```
+http://localhost:8000/
+```
--- scripts.js
+++ scripts.js
@@ -1,1 +1,70 @@
-console.log("Hello, World!");
+const cardContainer = document.getElementById("card-container");
+
+const tabs = document.querySelectorAll(".tab");
+
+let currentCategory = "prints";
+
+async function loadCategory(category) {
+  try {
+    const response = await fetch(`./data/${category}.json`);
+
+    const products = await response.json();
+
+    renderCards(products);
+  } catch (error) {
+    console.error("Failed loading category:", error);
+  }
+}
+
+function renderCards(products) {
+  cardContainer.innerHTML = "";
+
+  products.forEach((product) => {
+    const card = createCard(product);
+
+    cardContainer.appendChild(card);
+  });
+}
+
+function createCard(product) {
+  const card = document.createElement("a");
+
+  card.className = "card";
+
+  card.href = product.etsyLink;
+
+  card.target = "_blank";
+
+  card.innerHTML = `
+    <div class="image-wrapper">
+      <img
+        src="${product.image}"
+        alt="${product.title}"
+      />
+    </div>
+
+    <div class="card-content">
+
+      <h2>${product.title}</h2>
+
+      <p>${product.description}</p>s
+
+    </div>
+  `;
+
+  return card;
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((t) => t.classList.remove("active"));
+
+    tab.classList.add("active");
+
+    currentCategory = tab.dataset.file;
+
+    loadCategory(currentCategory);
+  });
+});
+
+loadCategory(currentCategory);
